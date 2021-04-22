e910e0b55bb4e0900bacaab513cbdf3364b9d90a
vijaya shree kanwal <kanwalvijayashree1809@gmail.com>
2021-04-22 15:33:40 +0530

    front-end

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,112 @@
 import React, { Component } from 'react'
-
- class About extends Component {
+import Images from '../images/image1.jpg';
+import "./about.css"
+ class Contacts extends Component {
     render() {
         return (
             <div>
-                <h2>This is about</h2>
+             
+
+<div class="bg-light">
+  <div class="container py-5">
+    <div class="row h-100 align-items-center py-5">
+      <div class="col-lg-6">
+        <h1 class="display-4">About us page</h1>
+        <p class="lead text-muted mb-0">We have made an expense trackker system that track it xyz</p>
+        
+      </div>
+      <div class="col-lg-6 d-none d-lg-block"><img src={Images}></img></div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-white py-5">
+  <div class="container py-5">
+    <div class="row align-items-center mb-5">
+      <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Anything about 1</h2>
+        <p class="font-italic text-muted mb-4"> morejidcszxnv</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm"></a>
+      </div>
+      <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834139/img-1_e25nvh.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></img> </div>
+    </div>
+    <div class="row align-items-center">
+      <div class="col-lg-5 px-5 mx-auto"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834136/img-2_vdgqgn.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></img> </div>
+      <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
+        <h2 class="font-weight-light">Anything about 2</h2>
+        <p class="font-italic text-muted mb-4">detail about me kuxh bhi likh dena related to your react app.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm"></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="bg-light py-5">
+  <div class="container py-5">
+    <div class="row mb-4">
+      <div class="col-lg-5">
+        <h2 class="display-4 font-weight-light">Our team</h2>
+        <p class="font-italic text-muted">Agar yha pr bhi u wann write any description so you can</p>
+      </div>
+    </div>
+
+    <div class="row text-center">
+     
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834132/avatar-4_ozhrib.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+          <h5 class="mb-0">Memmber 1 name</h5><span class="small text-uppercase text-muted">What he/she have done</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834130/avatar-3_hzlize.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+          <h5 class="mb-0">Gaurav singh kanwal</h5><span class="small text-uppercase text-muted">Made the about u and contact us</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+     
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-2_f8dowd.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+          <h5 class="mb-0">Team Memmber 3 name</h5><span class="small text-uppercase text-muted">xyz</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+     
+      <div class="col-xl-3 col-sm-6 mb-5">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://res.cloudinary.com/mhmd/image/upload/v1556834133/avatar-1_s02nlg.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"></img>
+          <h5 class="mb-0">Team memmber 4 name</h5><span class="small text-uppercase text-muted">more xyz</span>
+          <ul class="social mb-0 list-inline mt-3">
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+            <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+  
+
+    </div>
+  </div>
+</div>
+
+
+
             </div>
         )
     }
 }
-export default About;
+export default Contacts;
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
-
+import "./contact.css"
+import imgboys from '../images/boy.jpeg'
  class Contacts extends Component {
     render() {
         return (
             <div>
-                <h1>This is Contact</h1>
-                
+              <div class="container">
+   <div class="contact-parent">
+      <div class="contact-child child1">
+         <p>
+            <i class="fas fa-map-marker-alt"></i> Address <br />
+            <span> Ash Lane 110
+            <br />
+            London, UK
+            </span>
+         </p>
+         <p>
+            <i class="fas fa-phone-alt"></i> Let's Talk <br />
+            <span> 0787878787</span>
+         </p>
+         <p>
+            <i class=" far fa-envelope"></i> General Support <br />
+            <span>contact@example.com</span>
+         </p>
+      </div>
+      <div class="contact-child child2">
+         <div class="inside-contact">
+            <h2>Contact Us</h2>
+            <h3>
+               <span id="confirm"></span>
+            </h3>
+            <p>Name *</p>
+            <input id="txt_name" type="text" Required="required"></input>
+            <p>Email *</p>
+            <input id="txt_email" type="text" Required="required"></input>
+            <p>Phone *</p>
+            <input id="txt_phone" type="text" Required="required"></input>
+            <p>Subject *</p>
+            <input id="txt_subject" type="text" Required="required"></input>
+            <p>Message *</p>
+            <textarea id="txt_message" rows="4" cols="20" Required="required" ></textarea>
+            <input type="submit" id="btn_send" value="SEND"></input>
+         </div>
+      </div>
+   </div>
+</div>
             </div>
         )
     }
